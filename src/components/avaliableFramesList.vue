480260122cdc96b50a0aeefd1049b17d0671a9bb
Warat Wongmaneekit (Thang) <canopybanj@gmail.com>
2020-10-14 21:35:12 +0700

    Add frame section

--- src/components/avaliableFramesList.vue
+++ src/components/avaliableFramesList.vue
@@ -4,15 +4,29 @@
       v-for="frame in availableFrames"
       :key="frame.url"
       class="frame-select-list"
-      @click="frameClick(frame)"
     >
-      <img
-        :src="frame.url"
-        :style="{verticalAlign: 'middle', maxWidth:'50px',maxHeight:'50px'}"
-        class="frame-img"
+      <div
+        v-if="frame.type === 'frame'"
+        @click="frameClick(frame)"
       >
-      <br>
-      {{ frame.url.replace('.svg','').replace('/images/frames/','') }}
+        <img
+          :src="frame.url"
+          :style="{
+            verticalAlign: 'middle',
+            maxWidth: '50px',
+            maxHeight: '50px',
+          }"
+          class="frame-img"
+        >
+        <br>
+        {{ frame.url.replace(".svg", "").replace("/images/frames/", "") }}
+      </div>
+      <div
+        v-if="frame.type === 'section-divider'"
+        class="divider"
+      >
+        {{ frame.text }}
+      </div>
     </div>
   </div>
 </template>
@@ -21,29 +35,29 @@
 import availableFrames from "@/config/frames.json";
 
 export default {
-  name: 'AvaliableFramesList',
+  name: "AvaliableFramesList",
   props: {
     frameDetail: {
       type: Object,
-      default: () => {}
-    }
+      default: () => {},
+    },
   },
   data() {
     return {
-      availableFrames: availableFrames
+      availableFrames: availableFrames,
     };
   },
   methods: {
     frameClick(frameDetail) {
       this.$emit("frameClick", frameDetail);
-    }
-  }
+    },
+  },
 };
 </script>
 <style>
 #frames-select-container {
   z-index: 99999;
-  background: #FFF;
+  background: #fff;
   scroll-snap-type: x mandatory;
   overflow-x: scroll;
   overflow-y: hidden;
@@ -51,6 +65,11 @@ export default {
   align-items: center;
   font-size: 10px;
   grid-row-start: 2;
+}
+.divider{
+  font-size: 18px;
+  text-align: center;
+  font-weight: bold;
 }
 .frame-select-list {
   scroll-snap-align: start;
